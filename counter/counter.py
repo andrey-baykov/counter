4e5e9b1edfded6acfa48c3877da2817f31fd49a6
andrey-baykov <baykov.andrey@gmail.com>
2022-07-04 15:37:09 -0700

    Commented loop in counter.py. Added tests with pytest.

--- counter/counter.py
+++ counter/counter.py
@@ -1,28 +1,8 @@
-def get_count(string) -> int:
-    try:
-        return archive[string]
-    except KeyError:
-        archive[string] = counter(string)
-        return archive[string]
+from collections import Counter
+from functools import lru_cache
 
 
+@lru_cache(maxsize=None)
 def counter(string) -> int:
-    letters = {}
-    output = 0
-    for letter in string:
-        try:
-            letters[letter] += 1
-        except KeyError:
-            letters[letter] = 1
-    for char in letters:
-        if letters[char] == 1:
-            output += 1
-    return output
-
-
-archive = {}
-'''
-while True:
-    user_input = input("Input string: > ")
-    exit() if user_input == '' else print(get_count(user_input))
-'''
+    letters = Counter(string)
+    return sum([letters[char] for char in letters if letters[char] == 1])
--- tests/test_counter.py
+++ tests/test_counter.py
@@ -1,3 +1,5 @@
+import pytest
+
 from counter import counter
 
 params = [("aaa", 0),
@@ -15,17 +17,6 @@ params = [("aaa", 0),
           ]
 
 
-def test_counter():
-    for key in params:
-        assert counter.counter(key[0]) == key[1]
-
-
-def test_get_count():
-    for key in params:
-        assert counter.get_count(key[0]) == key[1]
-
-
-def test_archive():
-    for key in params:
-        counter.get_count(key[0])
-    assert counter.archive == {'': 0, ' ': 1, 'aaa': 0, 'abb': 1, 'asd  f': 4, 'wweqqrt': 3, 'zzx sdf': 5}
+@pytest.mark.parametrize("test_input, expected", params)
+def test_counter(test_input, expected):
+    assert counter.counter(test_input) == expected
