ad2e6cdc8d0270fd81ecd7d000babadbd02d960d
andrey-baykov <baykov.andrey@gmail.com>
2022-07-06 08:36:30 -0700

    Added "main" function with CLI's parameters. Added tests for CLI with mock.

--- counter/collect_framework.py
+++ counter/collect_framework.py
@@ -1,3 +1,4 @@
+import argparse
 from collections import Counter
 from functools import lru_cache
 
@@ -9,7 +10,19 @@ def counter(string) -> int:
 
 
 def main(arguments):
-    if arguments.file_path:
-        with open(arguments.file_path, "r") as file:
-            return counter(file.read())
+    if arguments.file:
+        return get_string_from_file(arguments.file)
     return counter(arguments.string)
+
+
+def get_string_from_file(path):
+    with open(path, "r") as f:
+        return counter(f.read())
+
+
+if __name__ == "__main__":
+    argparse = argparse.ArgumentParser()
+    argparse.add_argument("--string")
+    argparse.add_argument("--file")
+    args = argparse.parse_args()
+    print(main(args))
--- tests/test_collect_framework.py
+++ tests/test_collect_framework.py
@@ -1,7 +1,6 @@
 import argparse
-
+from unittest import mock
 import pytest
-
 from counter import collect_framework
 
 params = [("aaa", 0),
@@ -18,25 +17,31 @@ params = [("aaa", 0),
           ("asd  f", 4)
           ]
 
-params_input = [("aa1", "../test_1.txt", 3),
-                ("aa1", "../test_2.txt", 7),
-                ("aa1", "../test_3.txt", 9),
-                ("aa1", None, 1),
-                ("hello", None, 3),
-                ("'hello world!'", None, 7),
-                (None, "../test_1.txt", 3),
-                (None, "../test_2.txt", 7),
-                (None, "../test_3.txt", 9),
-                (None, None, 0)
-                ]
-
 
 @pytest.mark.parametrize("test_input, expected", params)
 def test_counter(test_input, expected):
     assert collect_framework.counter(test_input) == expected
 
 
-@pytest.mark.parametrize("test_input_str, test_input_file, expected", params_input)
-def test_main(test_input_str, test_input_file, expected):
-    args = argparse.Namespace(string=test_input_str, file_path=test_input_file)
-    assert collect_framework.main(args) == expected
+@mock.patch('counter.collect_framework.get_string_from_file', return_value=7)
+def test_main_full(file_data):
+    args = argparse.Namespace(string="hello", file=file_data)
+    assert collect_framework.main(args) == 7
+
+
+@mock.patch('counter.collect_framework.get_string_from_file', return_value=5)
+def test_main_string(file_data):
+    args = argparse.Namespace(string="hello", file=None)
+    assert collect_framework.main(args) == 3
+
+
+@mock.patch('counter.collect_framework.get_string_from_file', return_value=5)
+def test_main_file(file_data):
+    args = argparse.Namespace(string=None, file=file_data)
+    assert collect_framework.main(args) == 5
+
+
+@mock.patch('counter.collect_framework.get_string_from_file', return_value=3)
+def test_main_none(file_data):
+    args = argparse.Namespace(string=None, file=None)
+    assert collect_framework.main(args) == 0
